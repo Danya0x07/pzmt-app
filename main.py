74a453ca64ea217dcb2e294d34240ccb5a38e308
Danya0x07 <dlef0xf8@gmail.com>
2022-11-13 00:08:26 +0300

    Сигналы соединены с методами-заглушками

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from PyQt5.QtWidgets import QApplication
 
 from view_controller import ViewController
 
+print('На сообщения вида QTextCursor::setPosition: Position X out of range\n'
+      'не обращаем внимание - это баг Qt')
 qApp = QApplication([])
 vc = ViewController(None)
 sys.exit(qApp.exec())
--- view_controller.py
+++ view_controller.py
@@ -12,7 +12,62 @@ class ViewController(QMainWindow):
         uic.loadUi('design.ui', self)
         self.app = app
         self.connect_signals()
+        self.lcdOutHz.display(0)
         self.show()
 
     def connect_signals(self):
-        pass
+        self.rdInputHz.clicked.connect(self.__rdInputHzClicked)
+        self.rdInputNotes.clicked.connect(self.__rdInputNotesClicked)
+
+        self.btnConnect.clicked.connect(self.__btnConnectClicked)
+        self.btnPlayFrequency.clicked.connect(self.__btnPlayFrequencyClicked)
+        self.btnNoSound.clicked.connect(self.__btnNoSoundClicked)
+        self.btnPlayMelody.clicked.connect(self.__btnPlayMelodyClicked)
+        self.btnClearTxt.clicked.connect(self.__btnClearTxtClicked)
+        self.btnRecordKeys.clicked.connect(self.__btnRecordKeysClicked)
+        self.btnVolumeSwitch.clicked.connect(self.__btnVolumeSwitchClicked)
+
+        self.chbEnableKeyboard.stateChanged.connect(self.__chbEnableKeyboardStateChanged)
+
+        self.actOpenFile.triggered.connect(self.__actOpenFileTriggered)
+        self.actSaveFile.triggered.connect(self.__actSaveFileTriggered)
+        self.actShowAbout.triggered.connect(self.__actShowAboutTriggered)
+
+    def __rdInputHzClicked(self):
+        print("__rdInputHzClicked")
+
+    def __rdInputNotesClicked(self):
+        print("__rdInputNotesClicked")
+
+    def __btnConnectClicked(self):
+        print("__btnConnectClicked")
+
+    def __btnPlayFrequencyClicked(self):
+        print("__btnPlayFrequencyClicked")
+
+    def __btnNoSoundClicked(self):
+        print("__btnNoSoundClicked")
+
+    def __btnPlayMelodyClicked(self):
+        print("__btnPlayMelodyClicked")
+
+    def __btnClearTxtClicked(self):
+        print("__btnClearTxtClicked")
+
+    def __btnRecordKeysClicked(self):
+        print("__btnRecordKeysClicked")
+
+    def __btnVolumeSwitchClicked(self):
+        print("__btnVolumeSwitchClicked")
+
+    def __chbEnableKeyboardStateChanged(self):
+        print("__chbEnableKeyboardStateChanged")
+
+    def __actOpenFileTriggered(self):
+        print("__actOpenFileTriggered")
+
+    def __actSaveFileTriggered(self):
+        print("__actSaveFileTriggered")
+
+    def __actShowAboutTriggered(self):
+        print("__actShowAboutTriggered")
